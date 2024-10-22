1b3a634a541c6e8cf7dd1cc0e1ec88c1eb0733e6
kstobiecki <stobiecki.kamil@gmail.com>
2024-10-22 16:14:25 +0200

    Initial Commit

---
+++ Unrocketry Rocket!Tests/Unrocketry_Rocket_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Unrocketry_Rocket_Tests.swift
+//  Unrocketry Rocket!Tests
+//
+//  Created by Kamil Stobiecki on 22/10/2024.
+//
+
+import Testing
+@testable import Unrocketry_Rocket_
+
+struct Unrocketry_Rocket_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
